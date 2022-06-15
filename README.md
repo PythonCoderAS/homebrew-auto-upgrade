dbf1527927e68dae20a4bd5b91d8123320b713be
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-15 07:08:37 -0400

    Add code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # homebrew-auto-upgrade
 Automatically upgrade applicable packages based on `brew livecheck --all`.
+
+## How to use
+
+1. Install `expect` via homebrew: `brew install expect`
+2. Run `unbuffer -p brew livecheck`
+3. Pipe output to this script
+
+Example command:
+
+```shell
+unbuffer -p brew livecheck --all | pipenv run python main.py
+```
---
+++ main.py
@@ -0,0 +1,33 @@
+from asyncio import create_subprocess_exec, create_task, gather, run
+from asyncio.subprocess import DEVNULL
+
+from aioconsole import ainput, aprint
+
+
+async def bump_item(item: str):
+    await aprint("Bumping", item)
+    await create_subprocess_exec("brew", "bump", item, stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL)
+
+
+def filter(line: str) -> bool:
+    return "==> " in line and line.split("==> ")[1].strip().startswith("\x1b[32m")
+
+
+async def main():
+    tasks = []
+    try:
+        while text := await ainput():
+            if not filter(text):
+                continue
+            else:
+                package = text.split(": ")[0].strip()
+                tasks.append(create_task(bump_item(package)))
+    except EOFError:
+        # We're done.
+        pass
+    finally:
+        await gather(*tasks)
+
+
+if __name__ == '__main__':
+    run(main())
